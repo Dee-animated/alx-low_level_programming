c6a41991e9efd6a70b7dfbfcb278101773aea675
Dee-animated <ishemidivine@gmail.com>
2022-09-12 14:35:29 -0700

    I sometimes suffer from insomia and when i cant fall asleep i play what i call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - print if the number is postive, zero, or negative
+* Description: using the main function
+* program that prints the alphabet in lowercase, followed by a new line
+* Return: 0
+*/
+int main(void)
+{
+char al;
+for (al = 'a' ; al <= 'z' ; al++)
+{
+putchar(al);
+}
+putchar('\n');
+return (0);
+}
