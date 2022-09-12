3155aac23e67c47211c0a2c7f0f0dcc8b6e770c7
Dee-animated <ishemidivine@gmail.com>
2022-09-12 14:51:23 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,8 +2,6 @@
 /**
 * main - print if the number is postive, zero, or negative
 *
-
-
 * Description: using the main function
 * this program prints prints the alphabet in lowercase, followed by a new line.
 * Return: 0
@@ -11,6 +9,7 @@
 int main(void)
 {
 char ch;
+{
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 if (ch != 'e' && ch != 'q')
