7b95d61c4090268f5f830a99be410bb8897265df
Dee-animated <ishemidivine@gmail.com>
2022-09-13 21:21:18 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,21 @@
 #include "main.h"
-
 /**
- * main - Entry point
+ *main - entry point
  *
- * Description : prints _putchar as a message
+ * Description : prints _putchar as a message.
  *
  * Return : 0 on success
  */
 int main(void)
 {
-	char p[8] = "_putchar";
-		int i = 0;
-		char c;
-		while (i <= 8)
-		{
-			c = p(i);
-			_putchar(c);
-			i++;
-		}
-	_putchar('/n');
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
