7b5dfff36f741c626ac0bba860ceb1f56c07a69d
Dee-animated <ishemidivine@gmail.com>
2022-09-14 17:28:14 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in loweercase
+ *
+ * return: void
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z');
+	{
+		_putchar (c);
+		c++;
+	}
+	_putchar('\n');
+}
