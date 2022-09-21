0d17f542800c6a48f2df945511f35942acedcdd2
Dee-animated <ishemidivine@gmail.com>
2022-09-20 17:43:05 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of n to 98
+ * @n: int variable to be updated
+ *
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+	_putchar('\n');
+}
