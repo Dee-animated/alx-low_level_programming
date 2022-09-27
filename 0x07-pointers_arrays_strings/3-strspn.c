2851c500c0c177d1e28c57a0a448810946be2b23
Dee-animated <ishemidivine@gmail.com>
2022-09-26 21:41:37 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,34 +1,35 @@
 #include "main.h"
-
 /**
- * _strspn - function that gets the length of a prefix substring
- * @s: The string to be searched
- * @accept: The prefix to be measured
- *
- * Return: The number of bytes in s which consist only of bytes 
- * from accept
- */
+* _strspn - return length of string that matches values consistently
+* @s: string to search
+* @accept: target matches
+* Return: number of bytes consecutively matched
+*/
+
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int bytes = 0;
-	int index;
+int i = 0, j;
+int matches = 0;
 
-	while (*s)
-	{
-		for (index = 0; accept[index]; index++)
-		{
-			if (*s == accept[index])
-			{
-				bytes++;
-				break;
-			}
+while (s[i] != '\0') /*iterate through string*/
+{
 
-			else if (accept[index + 1] == '\0')
-				return (bytes);
-		}
+for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
+{
+if (s[i] == accept[j]) /*record & break at first match*/
+{
+matches++;
+break;
+}
 
-		s++;
-	}
+if (accept[j + 1] == '\0' && s[i] != accept[j])
 
-	return (bytes);
+return (matches);/*return if idx doesn't match*/
+}
+
+i++;
+}
+
+return (matches); /* return num if all match till end */
+
 }
